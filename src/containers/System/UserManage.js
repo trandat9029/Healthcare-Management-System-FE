afe498ddc5e18f29815955a05444a72ad3badd75
trandat9029 <tranledatvp@gmail.com>
2025-08-21 21:56:44 +0700

    main: create layout display data user

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './userManage.scss';
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            arrUsers : [],
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if(response && response.errCode === 0){
+            this.setState({
+                arrUsers: response.users,
+            }); 
+        }  
     }
 
 
     render() {
+        console.log('check render:', this.state.arrUsers);
+        let arrUsers = this.state.arrUsers;
+        
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center'>Manage users with Onizuka</div>
+                <div className='users-table mt-3 mx-1'>
+                    <table>
+                        <tr>
+                            <th>Email</th>
+                            <th>firstName</th>
+                            <th>LastName</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {arrUsers.map((item, index) => {
+                            return(
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className='btn-edit'><i className="fa-solid fa-pen-to-square"></i></button>
+                                        <button className='btn-delete'><i className="fa-solid fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,11 @@ const handleLoginApi = (userEmail, userPassword) =>{
     return axios.post('/api/login', {email: userEmail, password: userPassword});
 }
 
-export { handleLoginApi }
+const getAllUsers = (inputId) =>{
+    return axios.get(`/api/get-all-users?id=${inputId}`, {id: inputId})
+}
+ 
+export { 
+    handleLoginApi,
+    getAllUsers 
+}
