4724d5dc8e3e73dd194c86c5844bb6f58e05d294
trandat9029 <tranledatvp@gmail.com>
2025-08-26 23:05:41 +0700

    create layout header and add convert language vi-en

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,8 +18,9 @@ import Login from "./Auth/Login";
 import Header from "./Header/Header";
 import System from "../routes/System";
 
+import HomePage from './HomePage/HomePage.js';
+
 import { CustomToastCloseButton } from "../components/CustomToast";
-import ConfirmModal from "../components/ConfirmModal";
 
 class App extends Component {
   handlePersistorState = () => {
@@ -45,7 +46,6 @@ class App extends Component {
       <Fragment>
         <Router history={history}>
           <div className="main-container">
-            <ConfirmModal />
             {this.props.isLoggedIn && <Header />}
 
             <span className="content-container">
@@ -58,6 +58,10 @@ class App extends Component {
                 <Route
                   path={path.SYSTEM}
                   component={userIsAuthenticated(System)}
+                />
+                <Route
+                  path={path.HOMEPAGE}
+                  component={HomePage}
                 />
               </Switch>
             </span>
---
+++ src/containers/HomePage/HomeHeader.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+import "./HomeHeader.scss";
+import logo from '../../assets/logo.svg'
+
+class HomeHeader extends Component {
+  render() {
+    return (
+      <>
+        <div className="home-header-container">
+          <div className="home-header-content">
+            <div className="left-content">
+              <i className="fa-solid fa-bars"></i>
+              <img className="header-logo" src={logo} alt="logo" />
+            </div>
+            <div className="center-content">
+              <div className="child-content">
+                <h4>
+                  <b><FormattedMessage id="homeHeader.specialty" /></b>
+                </h4>
+                <p className="subs-desc"><FormattedMessage id="homeHeader.search-doctor" /></p>
+              </div>
+              <div className="child-content">
+                <h4>
+                  <b><FormattedMessage id="homeHeader.health-facility" /></b>
+                </h4>
+                <p className="subs-desc"><FormattedMessage id="homeHeader.select-room" /></p>
+              </div>
+              <div className="child-content">
+                <h4>
+                  <b><FormattedMessage id="homeHeader.doctor" /></b>
+                </h4>
+                <p className="subs-desc"><FormattedMessage id="homeHeader.select-doctor" /></p>
+              </div>
+              <div className="child-content">
+                <h4>
+                  <b><FormattedMessage id="homeHeader.fee" /></b>
+                </h4>
+                <p className="subs-desc"><FormattedMessage id="homeHeader.check-health" /></p>
+              </div>
+            </div>
+            <div className="right-content">
+              <div className="support">
+                <i className="fa-solid fa-circle-question"></i>
+                <FormattedMessage id="homeHeader.support" />
+              </div>
+              <div className="language-vi">VI</div>
+              <div className="language-en">EN</div>
+            </div>
+          </div>
+        </div>
+        <div className="home-header-banner">
+          <div className="content-up">
+            <div className="title1"><FormattedMessage id="banner.title1" /></div>
+            <div className="title2"><FormattedMessage id="banner.title2" /></div>
+            <div className="search">
+              <i className="fa-solid fa-magnifying-glass"></i>
+              <input type="text" placeholder="Tìm chuyên khoa khám bệnh"/>
+            </div>
+          </div>
+          <div className="content-down">             
+            <div className="options">
+              <div className="option-child">
+                <div className="icon-child"><i className="fa-regular fa-hospital"></i></div>
+                <div className="text-child"><FormattedMessage id="banner.child1" /></div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child"><i className="fa-solid fa-mobile-screen-button"></i></div>
+                <div className="text-child"><FormattedMessage id="banner.child2" /></div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child"><i className="fa-solid fa-hospital-user"></i></div>
+                <div className="text-child"><FormattedMessage id="banner.child3" /></div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child"><i className="fa-solid fa-microscope"></i></div>
+                <div className="text-child"><FormattedMessage id="banner.child4" /></div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child"><i className="fa-solid fa-user-doctor"></i></div>
+                <div className="text-child"><FormattedMessage id="banner.child5" /></div>
+              </div>
+              <div className="option-child">
+                <div className="icon-child"><i className="fa-solid fa-tooth"></i></div>
+                <div className="text-child"><FormattedMessage id="banner.child6" /></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language
+
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
