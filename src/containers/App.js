b85f5b6c4f179d7ac09985a7c815521144ca529d
trandat9029 <tranledatvp@gmail.com>
2025-12-23 09:18:55 +0700

    main: update reload

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter as Router } from "connected-react-router";
-import { history } from "../redux";
+import { history, persistor } from "../redux";
 import { ToastContainer } from "react-toastify";
 
 import {
@@ -34,6 +34,7 @@ import ClientLayout from "./Patient/ClientLayout";
 import Histories from "./Patient/History/Histories";
 import HandbookDetail from "./Patient/Handbook/HandbookDetail";
 import VerifyEmailCancel from "./Patient/VerifyEmailCancel";
+import { PersistGate } from "redux-persist/integration/react";
 
 class App extends Component {
   handlePersistorState = () => {
@@ -59,130 +60,131 @@ class App extends Component {
       <Fragment>
         <Router history={history}>
           <div className="main-container">
-            <div className="content-container">
-              <CustomScrollbars style={{ height: "100vh", width: "100%" }}>
-                <Switch>
-                  {/* trang hệ thống, không dùng header bệnh nhân */}
-                  <Route path={path.HOME} exact component={Home} />
-                  <Route
-                    path={path.LOGIN}
-                    component={userIsNotAuthenticated(Login)}
-                  />
-                  <Route path={path.SYSTEM} component={userIsAdmin(System)} />
-                  <Route path={path.DOCTOR} component={userIsDoctor(Doctor)} />
+            <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+              <div className="content-container">
+                <CustomScrollbars style={{ height: "100vh", width: "100%" }}>
+                  <Switch>
+                    {/* trang hệ thống, không dùng header bệnh nhân */}
+                    <Route path={path.HOME} exact component={Home} />
+                    <Route
+                      path={path.LOGIN}
+                      component={userIsNotAuthenticated(Login)}
+                    />
+                    <Route path={path.SYSTEM} component={userIsAdmin(System)} />
+                    <Route path={path.DOCTOR} component={userIsDoctor(Doctor)} />
 
-                  {/* các trang public cho bệnh nhân, dùng ClientLayout */}
-                  <Route
-                    path={path.HOMEPAGE}
-                    exact
-                    render={(props) => (
-                      <ClientLayout showBanner={true}>
-                        <HomePage {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    {/* các trang public cho bệnh nhân, dùng ClientLayout */}
+                    <Route
+                      path={path.HOMEPAGE}
+                      exact
+                      render={(props) => (
+                        <ClientLayout showBanner={true}>
+                          <HomePage {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path="/specialties"
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <Specialties {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path="/specialties"
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <Specialties {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path="/clinics"
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <Clinics {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path="/clinics"
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <Clinics {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path="/doctors"
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <Doctors {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path="/doctors"
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <Doctors {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path="/handbooks"
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <Handbooks {...props} />
-                      </ClientLayout>
-                    )}
-                  />
-                  <Route
-                    path="/histories"
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <Histories {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path="/handbooks"
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <Handbooks {...props} />
+                        </ClientLayout>
+                      )}
+                    />
+                    <Route
+                      path="/histories"
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <Histories {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path={path.DETAIL_DOCTOR}
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <DetailDoctor {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path={path.DETAIL_DOCTOR}
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <DetailDoctor {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path={path.DETAIL_SPECIALTY}
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <DetailSpecialty {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path={path.DETAIL_SPECIALTY}
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <DetailSpecialty {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path={path.DETAIL_CLINIC}
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <DetailClinic {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path={path.DETAIL_CLINIC}
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <DetailClinic {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path={path.DETAIL_HANDBOOK}
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <HandbookDetail {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path={path.DETAIL_HANDBOOK}
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <HandbookDetail {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path={path.VERIFY_EMAIL_BOOKING}
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <VerifyEmail {...props} />
-                      </ClientLayout>
-                    )}
-                  />
+                    <Route
+                      path={path.VERIFY_EMAIL_BOOKING}
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <VerifyEmail {...props} />
+                        </ClientLayout>
+                      )}
+                    />
 
-                  <Route
-                    path={path.VERIFY_EMAIL_CANCEL_BOOKING}
-                    render={(props) => (
-                      <ClientLayout showBanner={false}>
-                        <VerifyEmailCancel {...props} />
-                      </ClientLayout>
-                    )}
-                  />
-                </Switch>
-              </CustomScrollbars>
-            </div>
-
+                    <Route
+                      path={path.VERIFY_EMAIL_CANCEL_BOOKING}
+                      render={(props) => (
+                        <ClientLayout showBanner={false}>
+                          <VerifyEmailCancel {...props} />
+                        </ClientLayout>
+                      )}
+                    />
+                  </Switch>
+                </CustomScrollbars>
+              </div>
+            </PersistGate>
             <ToastContainer
               position="top-right"
               autoClose={5000}
